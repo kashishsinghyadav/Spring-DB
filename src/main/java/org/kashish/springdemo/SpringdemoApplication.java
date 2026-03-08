b095ff575a989d543ae11bfc40e2f1509b3b2db0
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-08 10:39:33 +0530

    added component annotaion for automap with object

--- src/main/java/org/kashish/springdemo/SpringdemoApplication.java
+++ src/main/java/org/kashish/springdemo/SpringdemoApplication.java
@@ -1,6 +1,5 @@
 package org.kashish.springdemo;
 
-import org.springframework.boot.ApplicationContextFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
