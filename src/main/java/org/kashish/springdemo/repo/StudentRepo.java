77aba3851f13428036862a3f2822f66cccc325f5
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-08 17:19:58 +0530

    create data and schema in properties file and make connection with h2

--- src/main/java/org/kashish/springdemo/repo/StudentRepo.java
+++ src/main/java/org/kashish/springdemo/repo/StudentRepo.java
@@ -2,14 +2,28 @@ package org.kashish.springdemo.repo;
 
 
 import org.kashish.springdemo.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentRepo {
 
+
+    private JdbcTemplate jdbcTemplate;
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
     public void save(Student student) {
-        System.out.println("Student saved");
+        String sql = "Insert into student (age,gender,name) values(?,?,?)";
+      int row=  jdbcTemplate.update(sql,student.getAge(),student.getGender(),student.getName());
+        System.out.println(row + "affected");
     }
 
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create  table  student(
+    name varchar(10) primary key ,
+    age int ,
+    gender varchar(10)
+);
