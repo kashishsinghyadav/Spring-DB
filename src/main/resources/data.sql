28a65d5f81f0a72220d6e4ae3f21f572529513cd
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-09 21:41:13 +0530

    added tomcat dependency

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into student (age,gender,name)  values (21,'female','Alice');
-insert into student (age,gender,name)  values (42,'female','Bob');
-insert into student (age,gender,name)  values (32,'male','Rishi');
--- src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-create  table  student(
-    name varchar(10) primary key ,
-    age int ,
-    gender varchar(10)
-);
