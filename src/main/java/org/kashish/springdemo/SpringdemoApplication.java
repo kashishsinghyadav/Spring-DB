910465b08a7c421b02a870705f5965e08e3e3d4d
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-09 14:18:36 +0530

    configure tomacat-servalet

--- src/main/java/org/kashish/springdemo/SpringdemoApplication.java
+++ src/main/java/org/kashish/springdemo/SpringdemoApplication.java
@@ -1,7 +1,5 @@
 package org.kashish.springdemo;
 
-import org.kashish.springdemo.model.Student;
-import org.kashish.springdemo.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,14 +10,14 @@ public class SpringdemoApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringdemoApplication.class, args);
 
-      Student student = context.getBean(Student.class);
-        StudentService studentService = context.getBean(StudentService.class);
-      student.setName("ashish");
-      student.setAge(23);
-      student.setGender("Female");
-
-      studentService.addStudent(student);
-        studentService.getStudent();
+//      Student student = context.getBean(Student.class);
+//        StudentService studentService = context.getBean(StudentService.class);
+//      student.setName("ashish");
+//      student.setAge(23);
+//      student.setGender("Female");
+//
+//      studentService.addStudent(student);
+//        studentService.getStudent();
 
     }
 
