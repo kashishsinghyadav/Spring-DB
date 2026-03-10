75db3e1e909a91e833200305804c1410098a25ef
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-10 22:05:58 +0530

    adding the RequestParam annotation for the accessing user data for nay variable name

--- src/main/java/org/kashish/springdemo/HomeController.java
+++ src/main/java/org/kashish/springdemo/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -15,11 +16,11 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(int num1,int num2, HttpSession session) {
+    public String add(@RequestParam("num1") int num, int num2, HttpSession session) {
         System.out.println("Add Controller called");
 //        int num1 = Integer.parseInt(request.getParameter("num1"));
 //        int num2 = Integer.parseInt(request.getParameter("num2"));
-        int result = num1 + num2;
+        int result = num + num2;
         session.setAttribute("result", result);
         return "Result.jsp";
     }
