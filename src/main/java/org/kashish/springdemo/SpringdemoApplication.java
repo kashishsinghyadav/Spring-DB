d46b55bdcddd30e84dfe41d0e749c63e7b48329c
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-03-10 18:38:01 +0530

    added tomcat dependency

--- src/main/java/org/kashish/springdemo/SpringdemoApplication.java
+++ src/main/java/org/kashish/springdemo/SpringdemoApplication.java
@@ -1,8 +1,6 @@
 package org.kashish.springdemo;
 
-import org.kashish.springdemo.model.Alein;
-import org.kashish.springdemo.model.Laptop;
-import org.kashish.springdemo.service.LaptopService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,12 +11,6 @@ public class SpringdemoApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringdemoApplication.class, args);
 
-        LaptopService servive = context.getBean(LaptopService.class);
-        Laptop laptop =context.getBean(Laptop.class);
-
-        servive.addLaptop(laptop);
-//        Alein obj =context.getBean(Alein.class);
-//        obj.code();
 
     }
 
